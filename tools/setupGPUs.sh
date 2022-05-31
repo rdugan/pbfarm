cd3b0a7a9d37d6ebf8f0b4f3f87216378cccf07a
rdugan <1779672+rdugan@users.noreply.github.com>
2022-05-31 00:37:37 -0700

    Test for settings, and use more generic instance id

--- tools/setupGPUs.sh
+++ tools/setupGPUs.sh
@@ -15,7 +15,7 @@
 #   limitations under the License.
 
 if [ $# != 2 ] || \
-   [[ ! $1 =~ ^([0-9](,[0-9])*|all)$ ]] || \
+   [[ ! $1 =~ ^([0-9]+(,[0-9]+)*|all)$ ]] || \
    [[ ! $2 =~ ^(mw29|mw31|cng|cnv2|cnh|cnt|ccx|eth|nim|kawpow|ergo)$ ]];
 then
   echo "$0 <N[,N]>|all mw29|mw31|cng|cnv2|cnh|cnt|ccx|eth|nim|kawpow|ergo"
@@ -31,15 +31,18 @@ ALGO=$COIN
 CONFIG_FILE="../config/json/$ALGO.json"
 PPT_DIR="../config/ppts"
 
-declare -a GPUS
+declare -a DRM_IDS
+declare -a ARCHS
 declare -A STATES
 declare -A TIMINGS
 declare -A ATITOOL_SETTINGS
 
+readarray -t DRM_IDS <<< `./PCI_DRM_map.sh |cut -d' ' -f2`
+
 if [[ $DEVICES =~ ^[0-9] ]]; then
-  GPUS=(${DEVICES//,/ })
+  DEVICES=(${DEVICES//,/ })
 else
-  readarray -t GPUS <<< `./PCI_DRM_map.sh |cut -d' ' -f2`
+  DEVICES=($(seq 0 1 $(( ${#DRM_IDS[@]}-1 ))))
 fi
 
 config=`jq '.' $CONFIG_FILE`
@@ -55,21 +58,24 @@ defaultTimings=`jq '.configDefaults.timings' <<< "$config"`
 defaultStates=`jq '.configDefaults.states' <<< "$config"`
 defaultVoltages=`jq '.configDefaults.voltages' <<< "$config"`
 
-for i in "${GPUS[@]}"
+for i in "${DEVICES[@]}"
 do
-  deviceConfig=`jq --argjson i "$i" '.devices[] | select(.drmID == $i)' $CONFIG_FILE`
+  deviceConfig=`jq --argjson i "$i" '.devices[] | select(.id == $i)' $CONFIG_FILE`
 
   HR_TARGET=`jq '.config? | .hashrateTarget' <<< "$deviceConfig"`
-  [[ ! -z $HR_TARGET ]] && HR_TARGET=$defaultHashrate
+  [[ -z $HR_TARGET || $HR_TARGET = "null" ]] && HR_TARGET=$defaultHashrate
   HR_TARGET="${HR_TARGET//./$''}"
 
-  DRM_ID=$i
-  I2C_ID=`jq '.i2cID' <<< "$deviceConfig"`
-  GPU_ARCH=`jq '.arch' <<< "$deviceConfig"`
+  INSTANCE_ID=`jq '.id' <<< "$deviceConfig"`
+
+  i2cID=`jq '.i2cID' <<< "$deviceConfig"`
+
+  gpuArch=`jq '.arch' <<< "$deviceConfig"`
+  ARCHS[$i]="$gpuArch"
 
   # get device specific ppt filename template if exists
   pptFile=`jq '.config? | .pptFile' <<< "$deviceConfig"`
-  [[ ! -z $pptFile ]] && pptFile=$defaultPPTFile
+  [[ -z $pptFile || $pptFile = "null" ]] && pptFile=$defaultPPTFile
   # get full filename by populating placeholders
   eval PPTS[$i]="$pptFile"
 
@@ -78,54 +84,71 @@ do
   # merge default and device timings
   timings=`echo "$defaultTimings $deviceTimings" | jq -s add`
   # build timings string
-  for k in `jq -r '. | keys[]' <<< "$timings"`
-  do
-    v=`jq --arg k "$k" -r '.[$k]' <<< "$timings"`
-    TIMINGS[$i]+="--$k $v "
-  done
+  if [[ ! -z $timings && "$timings" != "null" ]]; then
+    for k in `jq -r '. | keys[]' <<< "$timings"`
+    do
+      v=`jq --arg k "$k" -r '.[$k]' <<< "$timings"`
+      TIMINGS[$i]+="--$k $v "
+    done
+  fi
 
   # get device specific DPM states
   deviceStates=`jq '.config? | .states' <<< "$deviceConfig"`
   # merge default and device states
   states=`echo "$defaultStates $deviceStates" | jq -s add`
-  # extract core, mem, and soc state values from object
-  states=`jq -r '.core, .mem, .soc' <<< "$states"`
-  # convert jq string output to array
-  states=($states)
-  # add states to device array, stripping soc if null
-  STATES[$i]=${states[@]/null}
+  if [[ ! -z $states && "$states" != "null" ]]; then
+    # extract core, mem, and soc state values from object
+    states=`jq -r '.core, .mem, .soc' <<< "$states"`
+    # convert jq string output to array
+    states=($states)
+    # add states to device array, stripping soc if null
+    STATES[$i]=${states[@]/null}
+  fi
 
   # get device specific atitool voltages
   deviceVoltages=`jq '.config? | .voltages' <<< "$deviceConfig"`
   # merge default and device voltages
   voltages=`echo "$defaultVoltages $deviceVoltages" | jq -s add`
   # build voltages string
-  for k in `jq -r '. | keys[]' <<< "$voltages"`
-  do
-    v=`jq --arg k "$k" -r '.[$k]' <<< "$voltages"`
-    if [[ $k =~ vddcr_hbm ]] && [[ $GPU_ARCH =~ "Vega 20" ]]; then
-      # mvddc control doesn't work in atitool - need to use i2c instead
-      mvddc_increment="0.00625"
-      mvddc_index=`echo "obase=16; ($v-$mvddc_increment)/($mvddc_increment*2)" | bc`
-      I2C_SETTINGS[$i]="$I2C_ID 0x32 0xe3 0x${mvddc_index}"
-    else
-      ATITOOL_SETTINGS[$i]+="-$k=$v "
-    fi
-  done
+  if [[ ! -z $voltages && "$voltages" != "null" ]]; then
+    for k in `jq -r '. | keys[]' <<< "$voltages"`
+    do
+      v=`jq --arg k "$k" -r '.[$k]' <<< "$voltages"`
+      if [[ $k =~ vddcr_hbm && $gpuArch =~ "Vega 20" ]]; then
+        # mvddc control doesn't work in atitool - need to use i2c instead
+        mvddc_increment="0.00625"
+        mvddc_index=`echo "obase=16; ($v-$mvddc_increment)/($mvddc_increment*2)" | bc`
+        I2C_SETTINGS[$i]="$i2cID 0x32 0xe3 0x${mvddc_index}"
+      else
+        ATITOOL_SETTINGS[$i]+="-$k=$v "
+      fi
+    done
+  fi
 done
 
-for i in "${GPUS[@]}"
+# NOTE: amdmemtweak and atitool use contiguous instance IDs, all other tools
+#       use DRM IDs, which may not be contiguous due to the presence of 
+#       non-AMDGPU devices
+for i in "${DEVICES[@]}"
 do
+  DRM_ID=${DRM_IDS[$i]}
+
+  [[ "${ARCHS[$i]:-}" =~ (Vega 20|Navi) ]] && ./updatePPFeatures.sh $DRM_ID -UCLK_DPM
+
   # apply soft powerplay tables
-  [[ ! -z "${PPTS[$i]:-}" ]] && ./setPPT.sh $i "${PPT_DIR}/${PPTS[$i]}"
+  [[ ! -z "${PPTS[$i]:-}" ]] && ./setPPT.sh $DRM_ID "${PPT_DIR}/${PPTS[$i]}"
 
   # set powerplay states and mem timings
-  [[ ! -z "${STATES[$i]:-}" ]] && ./setGPUState.sh $i ${STATES[$i]}
+  [[ ! -z "${STATES[$i]:-}" ]] && ./setGPUState.sh $DRM_ID ${STATES[$i]}
   [[ ! -z "${TIMINGS[$i]:-}" ]] && ./amdmemtweak --i $i ${TIMINGS[$i]}
 
   # non-standard voltage settings
   [[ ! -z "${I2C_SETTINGS[$i]:-}" ]] && i2cset -y -r ${I2C_SETTINGS[$i]}
   [[ ! -z "${ATITOOL_SETTINGS[$i]:-}" ]] && ./atitool -i=$i ${ATITOOL_SETTINGS[$i]}
 
-#  ./disable_ecc.sh $i
+  ./initFans.sh $DRM_ID 1
+
+#  ./updatePPFeatures.sh $DRM_ID -ECC
+#  ./updatePPFeatures.sh $DRM_ID +LINK_DPM
+#  ./updatePPFeatures.sh $DRM_ID +FAN_CONTROL
 done
