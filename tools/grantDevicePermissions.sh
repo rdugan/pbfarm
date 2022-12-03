2ff9624d31870ee62983a8ce290a8bb469e931db
rdugan <1779672+rdugan@users.noreply.github.com>
2022-12-02 21:38:55 -0800

    New script to allow non-permissioned running of miner

---
+++ tools/grantDevicePermissions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#   Copyright 2022 rdugan
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+if [ $# -lt 1 ];
+then
+  echo "Usage: $0 <GPU_ID>"
+  echo "Example: $0 1"
+  exit 1;
+fi
+
+cd $(dirname $0)
+
+sysfs="/sys/class/drm/card${1}/device"
+
+# change permissions on resource5 file so we don't need to run
+# miner as root
+chown root.miners $sysfs/resource5
+chown root.miners $sysfs/resource5
+chmod g+rw $sysfs/resource5
+chmod g+rw $sysfs/resource5
+
